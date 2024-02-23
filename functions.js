315f193d63fa4e813644c41998974620202ff3d8
Sobia Fatima <133638285+sobiafatima20@users.noreply.github.com>
2024-02-23 20:56:05 +0500

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,15 @@
+//Functions
+//1. namefunction
+function funcname() {
+    console.log("never leave me along.");
+}
+funcname();
+function funcName() {
+    return "Sobia";
+}
+var name2 = funcName();
+function functName() {
+    return "Sobia";
+}
+var name3 = functName();
+console.log(name3);
---
+++ functions.ts
@@ -0,0 +1,22 @@
+//Functions
+//1. namefunction
+function funcname(){
+    console.log("never leave me along.");
+
+}
+funcname()
+
+function funcName(){
+    return  "Sobia";
+
+}
+
+let name2 : string = funcName()
+
+function functName(){
+    return  "Sobia";
+
+}
+
+let name3 : string = functName()
+console.log(name3)
